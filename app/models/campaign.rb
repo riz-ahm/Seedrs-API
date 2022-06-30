41112afc87be133338315a327543adfb4f9c11c2
riz-ahm <67613881+riz-ahm@users.noreply.github.com>
2022-07-01 05:06:53 +0600

    add seeds

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,7 +2,7 @@ class Campaign < ApplicationRecord
   has_many :investments, dependent: :destroy
 
   validates :name, presence: true
-  validates :percentage_raised, presence: true, numericality: { greater_than: 0 }
+  validates :percentage_raised, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :target_amount, presence: true, numericality: { greater_than: 0 }
   validates :sector, presence: true
   validates :country, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+image_url = 'http://some-url-to-a-pic.jpg'
+sector_list = ['clothing', 'food']
+50.times do |iteration|
+  sector = iteration % 2 == 0 ? sector_list.first : sector_list.second
+  puts "Creating campaign ##{iteration}"
+  Campaign.create(
+    name: "Campaign ##{iteration}",
+    image_url: image_url,
+    percentage_raised: 0.0,
+    target_amount: iteration * 5000,
+    sector: sector,
+    country: 'UK',
+    investment_multiple: iteration * 250,
+  )
+end
